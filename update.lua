5516d1f6daac1b53330611c5d841400f2f56d89d
nostripledev <52100294+nostripledev@users.noreply.github.com>
2021-10-20 11:52:39 +0200

    Update update.lua

--- update.lua
+++ update.lua
@@ -1,14 +1,19 @@
 local GizehVersion = GizehVersion or {}
 
-GizehVersion.Version=""--this variable will be overwrite by the loader
+GizehVersion.LoaderVersion=0--this variable will be overwrite by the loader this value is 1521
+GizehVersion.Version=""--this variable will be overwrite by the loader this value is 0.0.1
 GizehVersion.Check=false
-GizehVersion.Message="Votre Gizeh Est A Jour"
+GizehVersion.MessageSucess="Votre Gizeh Est A Jour"
 GizehVersion.MessageError="Votre Gizeh N'est Pas A Jour Viens Sur Le Discord"
 GizehVersion.Discord=""--this variable will be overwrite by the loader
 
 
-if GizehVersion.Version == "0.0.2" then
-    print(GizehVersion.Message)
-else
-    print(GizehVersion.MessageError.." "..GizehVersion.Discord)
+if GizehVersion.Check==false then
+    if GizehVersion.Version == "0.0.1" then
+        print(GizehVersion.MessageSucess)
+        GizehVersion.Check=true
+    else
+        print(GizehVersion.MessageError.." "..GizehVersion.Discord)
+        GizehVersion.Check=false
+    end
 end
