325d2390510f33b8f6179131124c06ff145fdfbd
nostripledev <52100294+nostripledev@users.noreply.github.com>
2021-10-20 10:30:52 +0200

    Update update.lua

--- update.lua
+++ update.lua
@@ -1,4 +1,14 @@
 local GizehVersion = GizehVersion or {}
 
-GizehVersion="0.0.2"
-GizehVersion.Message="Votre Gizeh N'est Pas A Jour"
+GizehVersion.Version=""
+GizehVersion.Check=false
+GizehVersion.Message="Votre Gizeh Est A Jour"
+GizehVersion.MessageError="Votre Gizeh N'est Pas A Jour Viens Sur Le Discord"
+GizehVersion.Discord=""
+
+
+if GizehVersion.Version == "0.0.2" then
+    print(GizehVersion.Message)
+else
+    print(GizehVersion.MessageError.." "..GizehVersion.Discord)
+end
