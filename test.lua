a10217d83483f3c4c87476eb7448e5575e60acb3
nostripledev <52100294+nostripledev@users.noreply.github.com>
2021-10-20 11:34:03 +0200

    Update test.lua

--- test.lua
+++ test.lua
@@ -3,4 +3,24 @@ local GizehTest = GizehTest or {}
 GizehTest.Main = GizehTest.Main or {}
 GizehTest.Main.Message = "ceci est un message test pour gizeh"
 
+function FormatCode( lua, string ,args)
+	
+	lua = string.Replace(lua, "#STR", string)
+  lua = string.Replace(lua, "#ARGS", args)
+  lua = string.Replace(lua, "#CODE", code)
+	return lua
+end
+
+GizehTest.Main.Function.Add = function(string,args,code)
+local code = [[
+    #STR function(#ARG)
+        #CODE
+    end
+]]
+
+code = FormatCode(code,string,args,code)
+end
+
+GizehTest.Main.Function.Add("LaFonctionDeOuf","Argument","print(Argmuent..'c\'est ouf')")
+
 --le reste des tests seront fait ici
